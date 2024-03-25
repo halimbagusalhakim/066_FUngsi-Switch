6f5ab7ace598a3d5d6bb61243c1590599c132125
halimbagusalhakim <halsat132@gmail.com>
2024-03-25 14:38:12 +0700

    menampilkan display pilihan

--- calculator.cpp
+++ calculator.cpp
@@ -13,15 +13,28 @@ float penjumlahan(float a, float b){
 }
 float pembagian(float a, float b){
     return a / b;
-
+}
 float perkalian(float a, float b){
     return a * b;
-
+}
 float pengurangan (float a, float b){
     return a * b;
-
+}
 
 
 int main(){
+    int pilihan;
 
+    do{
+        cout << "==============" << endl;
+        cout << "Menu" << endl;
+        cout << "1. penjumlahan" << endl;
+        cout << "2. pembagian" << endl;
+        cout << "3. perkalian" << endl;
+        cout << "4. pengurangan" << endl;
+        cout << "5. exit" << endl;
+        cout << "masukan pilihan 1/2/3/4/5" << endl;
+        cin >> pilihan;
+         
+    }
 }
