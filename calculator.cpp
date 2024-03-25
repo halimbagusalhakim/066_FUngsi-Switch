9fee05474f382fb2ec91772aab869ea76ac4d5c9
halimbagusalhakim <halsat132@gmail.com>
2024-03-25 14:30:16 +0700

    membuat Prosedur input

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int bilSatu, bilDua;
+void input(){
+    cout << "masukan Bilangan Pertama = ";
+    cin >> bilSatu;
+    cout << "Masukan Bilangan Kedua =";
+    cin >> bilDua;
+}
 
 int main(){
 
